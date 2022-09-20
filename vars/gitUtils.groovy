95536f292a71771a2699641899829d7a7579ed69
Steven GUEGUEN <steven.gueguen@senx.io>
2022-09-20 17:58:40 +0200

    debuq

--- vars/gitUtils.groovy
+++ vars/gitUtils.groovy
@@ -1,9 +1,11 @@
 def getVersion() {
-    return sh(returnStdout: true, script: 'git describe --abbrev=0 --tags').trim()
+    echo "1.2.42"
+    // return sh(returnStdout: true, script: 'git describe --abbrev=0 --tags').trim()
 }
 
 def isTag() {
-    String lastCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-    String tag = sh(returnStdout: true, script: "git show-ref --tags -d | grep ^${lastCommit} | sed -e 's,.* refs/tags/,,' -e 's/\\^{}//'").trim()
-    return tag != ''
+    // String lastCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+    // String tag = sh(returnStdout: true, script: "git show-ref --tags -d | grep ^${lastCommit} | sed -e 's,.* refs/tags/,,' -e 's/\\^{}//'").trim()
+    // return tag != ''
+    return false
 }
