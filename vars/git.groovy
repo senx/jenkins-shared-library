86430f24273564e960e9134bcdfa16b9c0ce747c
Steven GUEGUEN <steven.gueguen@senx.io>
2022-09-20 17:51:22 +0200

    Add git function

---
+++ vars/git.groovy
@@ -0,0 +1,9 @@
+def getVersion() {
+    return sh(returnStdout: true, script: 'git describe --abbrev=0 --tags').trim()
+}
+
+def isTag() {
+    String lastCommit = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+    String tag = sh(returnStdout: true, script: "git show-ref --tags -d | grep ^${lastCommit} | sed -e 's,.* refs/tags/,,' -e 's/\\^{}//'").trim()
+    return tag != ''
+}
