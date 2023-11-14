0c60b2f2b72fa80a979cd598f2d08df3db40506b
Steven Gueguen <steven.gueguen@senx.io>
2023-11-14 16:14:37 +0100

    conditionnal publication

--- vars/moduleDeliveryPipeline.groovy
+++ vars/moduleDeliveryPipeline.groovy
@@ -9,6 +9,10 @@ def call(Map pipelineParams, Boolean deployOnWarpFleet=true) {
             timestamps()
         }
 
+        triggers {
+            pollSCM('H/15 * * * 1-5')
+        }
+
         environment {
             GPG_KEY_NAME = "${env.gpgKeyName}"
             NEXUS_HOST = "${env.nexusHost}"
@@ -52,39 +56,45 @@ def call(Map pipelineParams, Boolean deployOnWarpFleet=true) {
                 }
             }
 
-            stage('Publish to: Local Nexus and Maven Central') {
+            stage('Publish') {
                 when {
-                    beforeInput true
                     expression { gitUtils.isTag() }
                 }
-                options {
-                    timeout(time: 4, unit: 'DAYS')
-                }
-                input {
-                    message "Should we deploy module to 'Local Nexus' and 'Maven Central'?"
-                }
-                steps {
-                    sh "$GRADLE_CMD publish closeAndReleaseStagingRepository"
-                }
-            }
 
-            stage("Wait for module to be avaible on maven central") {
-                steps {
-                    // Wait 15 minutes for the components to be available on Maven Central
-                    sh "sleep 900"
-                    // Check if the jar is available on Maven Central, retry if failed
-                    sh "while ! curl -s -f https://repo1.maven.org/maven2/io/warp10/${env.JOB_NAME}/${env.version}/${env.JOB_NAME}-${env.version}.jar >/dev/null; do sleep 60; done"
+                stage('Publish to: Local Nexus and Maven Central') {
+                    when {
+                        beforeInput true
+                        expression { gitUtils.isTag() }
+                    }
+                    options {
+                        timeout(time: 4, unit: 'DAYS')
+                    }
+                    input {
+                        message "Should we deploy module to 'Local Nexus' and 'Maven Central'?"
+                    }
+                    steps {
+                        sh "$GRADLE_CMD publish closeAndReleaseStagingRepository"
+                    }
                 }
-            }
 
-            stage("Publish to WarpFleet") {
-                when {
-                    expression { deployOnWarpFleet }
+                stage("Wait for module to be avaible on maven central") {
+                    steps {
+                        // Wait 15 minutes for the components to be available on Maven Central
+                        sh "sleep 900"
+                        // Check if the jar is available on Maven Central, retry if failed
+                        sh "while ! curl -s -f https://repo1.maven.org/maven2/io/warp10/${env.JOB_NAME}/${env.version}/${env.JOB_NAME}-${env.version}.jar >/dev/null; do sleep 60; done"
+                    }
                 }
-                steps {
-                    nvm('version':'v16.18.0') {
-                        sh "wf publish --gpg=${env.warpfleetGPG} --gpgArg='--batch' ${env.version} https://repo.maven.apache.org/maven2"
-                        sh "wf publish --gpg=${env.warpfleetGPG} --gpgArg='--batch' ${env.version}-uberjar https://repo.maven.apache.org/maven2"
+
+                stage("Publish to WarpFleet") {
+                    when {
+                        expression { deployOnWarpFleet }
+                    }
+                    steps {
+                        nvm('version':'v16.18.0') {
+                            sh "wf publish --gpg=${env.warpfleetGPG} --gpgArg='--batch' ${env.version} https://repo.maven.apache.org/maven2"
+                            sh "wf publish --gpg=${env.warpfleetGPG} --gpgArg='--batch' ${env.version}-uberjar https://repo.maven.apache.org/maven2"
+                        }
                     }
                 }
             }
